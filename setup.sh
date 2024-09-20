1fda5b7d6cdc3bfc3057277ca9035debf1afa84b
HeroBuxx <me@herobuxx.me>
2024-09-20 17:41:51 +0700

    setup: Install few more packages
    Instal git-lfs, python3-full, and xmlstarlet
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- setup.sh
+++ setup.sh
@@ -50,14 +50,14 @@ sudo DEBIAN_FRONTEND=noninteractive \
     apt install \
     adb autoconf automake axel bc bison build-essential \
     ccache clang cmake expat fastboot flex g++ \
-    g++-multilib gawk gcc gcc-multilib git gnupg gperf \
+    g++-multilib gawk gcc gcc-multilib git git-lfs gnupg gperf \
     htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
     libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
     libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
     maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
-    pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+    pngquant python2.7 python-all-dev python3-full re2c schedtool squashfs-tools subversion \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
-    libxml-simple-perl apt-utils dwarves dialog x11-xserver-utils dnsutils lld libelf-dev jq axel \
+    libxml-simple-perl apt-utils dwarves dialog x11-xserver-utils dnsutils lld libelf-dev jq axel xmlstarlet \
     ${PACKAGES} ${EXTRA_PACKAGES} -y
 
 
